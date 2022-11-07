f356235ba86e2f1f7df506b09fcad81402bbea92
GautamRamani <gautam.ramani06@gmail.com>
2022-11-07 18:12:54 +0530

    commit

---
+++ Week-5/Modules/server.js
@@ -0,0 +1,17 @@
+// console.log("Hello...")
+// console.log(arguments)
+
+// console.log(__dirname)
+// console.log(__filename)
+
+// exports.name="Aryan Bhattcharya"
+// console.log(arguments)
+
+// module.exports.name = "James Bond";
+// console.log(arguments);
+
+// exports.name = "James Bond";
+// exports.job = "Accountant";
+// exports.age = 21;
+// console.log(arguments)
+
