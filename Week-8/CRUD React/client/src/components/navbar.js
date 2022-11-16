964490b2eb8b44e4749f5a9a4e61d2cf83fbc9d2
GautamRamani <gautam.ramani06@gmail.com>
2022-11-16 13:59:28 +0530

    commit

--- Week-8/CRUD React/client/src/components/navbar.js
+++ Week-8/CRUD React/client/src/components/navbar.js
@@ -1,37 +1,35 @@
 
 import { AppBar, Box, Toolbar, Typography, Button } from "@mui/material"
-import { useContext, useEffect } from "react"
-import { Link, Navigate } from "react-router-dom";
+import {useContext} from "react"
+import { Link } from "react-router-dom";
 import { AuthContext } from "../context/authContext";
-import { useNavigate } from "react-router-dom"
+import {useNavigate} from "react-router-dom"
 
 function Navbar() {
-    let navigate = useNavigate();
-    const { user, logout } = useContext(AuthContext)
-    useEffect(() => {
+    let navigate=useNavigate();
+    const {user,logout}=useContext(AuthContext)
 
-        console.log(user)
-    }, [])
-    const onLogout = () => {
+    const onLogout=()=>{
         logout();
         navigate("/");
     }
+    console.log(user)
     return (
         <Box sx={{ flexGrow: 1 }}>
             <AppBar position="static">
                 <Toolbar>
                     <Typography variant="h5" component="div">
-                        <Link to="/" style={{ textDescription: "none", color: "white" }}>ReactLogin</Link>
+                        <Link to="/" style={{textDescription:"none",color:"white"}}>ReactLogin</Link>
                     </Typography>
-                    <Box alignItems="right" sx={{ flexGrow: 1, textAlign: "right" }}>
-                        {user ?
+                    <Box alignItems="right" sx={{flexGrow:1,textAlign:"right"}}>
+                        {   user?
                             <>
-                                <Button style={{ textDescription: "none", color: "white" }} onClick={onLogout}>Logout</Button>
+                                <Button style={{textDescription:"none",color:"white"}}onClick={onLogout}>Logout</Button>
                             </>
                             :
                             <>
-                                <Link to="/login" style={{ textDescription: "none", color: "white", marginRight: "10px" }}>Login</Link>
-                                <Link to="/register" style={{ textDescription: "none", color: "white" }}>Register</Link>
+                                <Link to="/login" style={{textDescription:"none",color:"white",marginRight:"10px"}}>Login</Link>
+                                <Link to="/register" style={{textDescription:"none",color:"white"}}>Register</Link>
                             </>
                         }
                     </Box>
