15ba0ae3ed044e2a44f188796cb3fbb9083949cd
GautamRamani <gautam.ramani06@gmail.com>
2022-11-21 16:04:31 +0530

    commit

--- Week-8/Swagger/Demo/index.js
+++ Week-8/Swagger/Demo/index.js
@@ -33,7 +33,7 @@ app.use("/api-docs",swaggerUi.serve,swaggerUi.setup(openapiSpecification))
  * @swagger
  * /:
  *   get:
- *     summery: This api is used to check if get method s working or not
+ *     summary: This api is used to check if get method s working or not
  *     description: This api is used to check if get method s working or not
  *     responses:
  *       200:
@@ -47,7 +47,7 @@ app.get("/",(req,res)=>{
 /**
  * @swagger
  *  components:
- *      schema: 
+ *      schemas: 
  *          Course:
  *                type: object
  *                properties:
@@ -67,7 +67,7 @@ app.get("/",(req,res)=>{
  * @swagger
  * /api/courses:
  *  get: 
- *      summery: To get all courses from mongodb
+ *      summary: To get all courses from mongodb
  *      description: This api is used to fetch data from mongodb
  *      responses:
  *       200:
@@ -77,7 +77,7 @@ app.get("/",(req,res)=>{
  *                    schema:
  *                        type: array
  *                    items:
- *                        $ref:'#components/schema/Course
+ *                        $ref: '#components/schemas/Course'
  */
 app.get('/api/courses', async(req, res) => {
   try {
@@ -93,7 +93,7 @@ app.get('/api/courses', async(req, res) => {
  * @swagger
  * /api/course/{_id}:
  *  get: 
- *      summery: To get all courses from mongodb
+ *      summary: To get perticular course from mongodb
  *      description: This api is used to fetch data from mongodb
  *      parameters:
  *          - in: path
@@ -110,7 +110,7 @@ app.get('/api/courses', async(req, res) => {
  *                    schema:
  *                        type: array
  *                    items:
- *                        $ref:'#components/schema/Course
+ *                        $ref: '#components/schemas/Course'
  */
 app.get('/api/course/:id', async(req, res) => {
   try {
@@ -122,6 +122,125 @@ app.get('/api/course/:id', async(req, res) => {
   }
 })
 
+/**
+ * @swagger
+ * /api/addcourse:
+ *  post: 
+ *      summary: used to insert data to mongodb
+ *      description: This api is used to fetch data from mongodb
+ *      requestBody:
+ *          required: true
+ *          content: 
+ *              application/json:
+ *                  schema: 
+ *                      $ref: '#components/schemas/Course'
+ *      responses:
+ *       200:
+ *           description: Added Successfully
+ */
+
+ app.post('/api/addcourse', async(req, res) => {
+  try {
+
+      let courseadd = new Course({
+        name:req.body.name,
+        author:req.body.author,
+        tags:req.body.tags,
+        isPublished:req.body.isPublished
+      })
+      courseadd=await courseadd.save();
+
+    res.status(200).send(courseadd)
+  } catch (error) {
+    console.log(error)
+  }
+})
+
+/**
+ * @swagger
+ * /api/putcourse/{_id}:
+ *  put: 
+ *      summary: used to update data to mongodb
+ *      description: This api is used to fetch data from mongodb
+ *      parameters:
+ *          - in: path
+ *            name: _id
+ *            required: true
+ *            description: string ID required
+ *            schema:
+ *              type: string
+ *      requestBody:
+ *          required: true
+ *          content: 
+ *              application/json:
+ *                  schema: 
+ *                      $ref: '#components/schemas/Course'
+ *      responses:
+ *       200:
+ *           description: Added Successfully
+ *           content:
+ *               application/json:
+ *                    schema:
+ *                        type: array
+ *                    items:
+ *                        $ref: '#components/schemas/Course'
+ */
+
+  //put
+  app.put('/api/putcourse/:id',async(req, res) => {
+    try {  
+      const courseupdate=await Course.findByIdAndUpdate(req.params.id,
+        {
+        name:req.body.name,
+        author:req.body.author,
+        tags:req.body.tags,
+        isPublished:req.body.isPublished,
+        new:true
+      })
+    
+      if (!courseupdate) res.status(404).send('The genre with the given id is not available')
+      res.send(courseupdate);
+
+    } catch (error) {
+      console.log(error)
+    }
+  })
+
+/**
+ * @swagger
+ * /api/delcourse/{_id}:
+ *  delete: 
+ *      summary: this api is use to delete record from mongodb database 
+ *      description: This api is used to fetch data from mongodb
+ *      parameters:
+ *          - in: path
+ *            name: _id
+ *            required: true
+ *            description: string ID required
+ *            schema:
+ *              type: string
+ *      responses:
+ *       200:
+ *           description: This api is used to fetch data from mongodb
+ */
+  app.delete('/api/delcourse/:id',async (req, res) => {
+    try {
+      // deleting a record 
+      let _id= req.params.id;
+      await Course.findByIdAndDelete(_id)
+          .then((course) => {
+              if (!course) {
+                  res.status(404).send();
+              }
+              res.send(course);
+          }).catch((e) => {
+              res.status(400).send(e);
+          });
+    } catch (error) {
+      console.log(error)
+    }
+});
+  
 
 const PORT=1234;
 app.listen(PORT, () => {
