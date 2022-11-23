62d5db495e5e9fed4793f5b041478ef5a6279e39
GautamRamani <gautam.ramani06@gmail.com>
2022-11-23 17:10:40 +0530

    commit

--- Week-5/Aggregation/Aggregation.js
+++ Week-5/Aggregation/Aggregation.js
@@ -117,6 +117,7 @@
 //    ])
 
 // $unwind
+//it will Populate the array
 // Deconstructs an array field from the input documents to output a document for each element. Each output document is the input document with the value of the array field replaced by the element.
 // unwind and project
 // db.getCollection("aggregate").aggregate([
