b9ead71c4dcfa778a871917440a784066708af36
GautamRamani <gautam.ramani06@gmail.com>
2022-12-02 09:36:48 +0530

    commit

--- Z-Extra/ECommerce Structured/app.js
+++ Z-Extra/ECommerce Structured/app.js
@@ -1,29 +1,29 @@
-const express=require("express")
+const express = require("express")
 const bodyParser = require("body-parser")
 const morgan = require("morgan")
-const dotenv=require('dotenv').config()
-const cors=require("cors")
-const logger=require("./config/logger")
-const authJwt=require("./middleware/auth")
-const app=express()
+const dotenv = require('dotenv').config()
+const cors = require("cors")
+const logger = require("./config/logger")
+const authJwt = require("./middleware/auth")
+const app = express()
 
-const stream={
-    write:(messege)=>{
+const stream = {
+    write: (messege) => {
         logger.info(messege)
-    }   
+    }
 }
 //middleware
-app.use("/public/uploads",express.static(__dirname+"/public/uploads"))
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms',{stream}))
+app.use("/public/uploads", express.static(__dirname + "/public/uploads"))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms', { stream }))
 app.use(bodyParser.json())
 app.use(cors())
 // app.use(authJwt());
 
 require("./config/connection")
 
-const routeIndex=require("./Route/index")
-app.use("/",routeIndex);
+const routeIndex = require("./Route/index")
+app.use("/", routeIndex);
 
-app.listen(process.env.PORT,()=>{
+app.listen(process.env.PORT, () => {
     console.log(`Server Listening on PORT ${process.env.PORT}`)
 })
