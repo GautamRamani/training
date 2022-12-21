e4266f3d63ba2a01bcf22d8149049c3c793d78a6
Gautam Ramani <gautam.ramani@artoon.in>
2022-12-21 10:40:44 +0530

    commit

--- Week-5/TypeScript/1.Basic/dist/first.js
+++ Week-5/TypeScript/1.Basic/dist/first.js
@@ -1,17 +1,2 @@
 "use strict";
-class Account {
-    constructor() {
-        this._balance = 1000;
-    }
-    get balance() {
-        return this._balance;
-    }
-    set balanced(value) {
-        if (value < 0)
-            throw new Error();
-        this._balance = value;
-    }
-}
-let showbal = new Account();
-console.log(showbal.balance);
 //# sourceMappingURL=first.js.map
